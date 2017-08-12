802e9a1506859b80c5cc46449efa5521a2c1e64c
Nick Tomlin <nick.tomlin@gmail.com>
2017-08-12 16:20:48 -0500

    Code examples

--- dev/example.js
+++ dev/example.js
@@ -3,10 +3,13 @@ import { h, Component} from 'preact'
 // https://github.com/PrismJS/prism/issues/593#issuecomment-226143416
 const Prism = require('prismjs')
 require('prismjs/components/prism-jsx')
+require('prismjs/plugins/normalize-whitespace/prism-normalize-whitespace');
+
 
 export default function Example () {
   let examples = this.props.children.map((child) => {
-    let code = Prism.highlight(child, Prism.languages[this.props.syntax])
+    let normalized = Prism.plugins.NormalizeWhitespace.normalize(child)
+    let code = Prism.highlight(normalized, Prism.languages[this.props.syntax])
     return <code dangerouslySetInnerHTML={{__html: code}}></code>
   })
 
