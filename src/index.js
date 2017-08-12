effac26678748d8fb9aed71726e3e6edbd93b637
Nick Tomlin <nick.tomlin@gmail.com>
2017-08-12 11:28:24 -0500

    Update with compound structure

--- src/index.js
+++ src/index.js
@@ -1,28 +1,29 @@
-import { render, h, Component } from 'preact'
+import { render, h, Component, cloneElement } from 'preact'
 
 function getCoordinatesForPercent (percentage) {
-  const x = Math.cos(2 * Math.PI * percentage);
-  const y = Math.sin(2 * Math.PI * percentage);
+  const x = Math.cos(2 * Math.PI * percentage)
+  const y = Math.sin(2 * Math.PI * percentage)
 
-  return [x, y];
+  return [x, y]
 }
 
 export class Slice extends Component {
-  generatePath (cumulativePercent, slicePercent) {
-    const [startX, startY] = getCoordinatesForPercent(cumulativePercent)
-    const [endX, endY] = getCoordinatesForPercent(cumulativePercent + slicePercent)
+  generatePath (start, slicePercent) {
+    const [startX, startY] = getCoordinatesForPercent(start)
+    const [endX, endY] = getCoordinatesForPercent(start + slicePercent)
     const largeArcFlag = slicePercent > .5 ? 1 : 0
 
     return [
       `M ${startX} ${startY}`,
       `A 1 1 0 ${largeArcFlag} 1 ${endX} ${endY}`,
       `L 0 0`,
-    ].join(' ');
+    ].join(' ')
   }
 
   render () {
     return <path
-      d={this.generatePath(this.props.cumulativePercent, this.props.percent)}
+      {...this.props}
+      d={this.generatePath(this.props.start, this.props.percent)}
       fill={this.props.fill}
     />
   }
@@ -30,25 +31,33 @@ export class Slice extends Component {
 
 // this is the poor person's technique introduced by
 // https://hackernoon.com/a-simple-pie-chart-in-svg-dbdd653b6936
-export default class Pizza extends Component {
-  renderSlices(sliceData) {
+export default class Pie extends Component {
+  renderSlices() {
     let cumulativePercent = 0
 
-		return sliceData.map(slice => {
-      let elem = <Slice cumulativePercent={cumulativePercent} percent={slice.percent} fill={slice.fill}/>
-      cumulativePercent = cumulativePercent + slice.percent
-      return elem
-		})
+    return this.props.children.map((child) => {
+      let name = child.nodeName.name
+      if (name === 'Slice') {
+        let { percent, fill } = child.attributes
+        let start = cumulativePercent
+        cumulativePercent = cumulativePercent + percent
+
+        return cloneElement(child, {
+          start,
+          percent,
+          fill,
+        })
+      } else { return child }
+    })
   }
 
   render () {
-    const slices = this.props.data ? this.renderSlices(this.props.data) : null
     return (
       <svg
         style="transform: rotate(-90deg);"
         viewBox="-1 -1 2 2"
         >
-        {slices}
+        {this.renderSlices()}
       </svg>
     )
   }
